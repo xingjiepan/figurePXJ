c3b59cd5ac15405afd74d25b17779680e4408fd9
xingjiepan <xingjiepan@gmail.com>
2016-12-14 15:59:25 -0800

    Add a new color scheme, rainbow

--- figurePXJ/Style.py
+++ figurePXJ/Style.py
@@ -2,8 +2,34 @@
 
 styles = {
     'red_blue_black_light':{
-        'colors':['#F06449', '#5BC3EB', '#36382E', '#DADAD9', '#EDE6E3']
-    }
+        'colors':{
+            'normal':{
+                'basics':['#F06449', '#5BC3EB', '#DADAD9'],
+                'black':['#36382E'],
+                'white':['#EDE6E3'],
+            },
+            'intense':{
+                'basics':['#C5523C', '#4BA0C1', '#B3B3B2'],
+                'black':['#2D2E26'],
+                'white':['#C2BDBA'],
+            }
+        }
+    },
+    
+    'rainbow':{
+        'colors':{
+            'normal':{
+                'basics':['#46237A', '#256EFF', '#3DDC97', '#FCD0A1', '#FF495C'],
+                'black':['#000000'],
+                'white':['#FFFFFF'],
+            },
+            'intense':{
+                'basics':['#3A1D64', '#1F5BD1', '#32B57C', '#CFAB84', '#D13C4C'],
+                'black':['#000000'],
+                'white':['#FFFFFF'],
+            }
+        }
+    },
     
 }
 
@@ -13,5 +39,8 @@ class Style:
     def __init__(self, style_name):
         self.style = styles[style_name]
 
-    def color(self, color_index):
-        return self.style['colors'][color_index]
+    def color(self, family, genus, species=0):
+        return self.style['colors'][family][genus][species]
+    
+    def color_genus(self, family, genus):
+        return self.style['colors'][family][genus]
